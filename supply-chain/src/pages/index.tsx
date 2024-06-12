507327244ccae7253f00e3284e6bd107d33066f3
Richard Abishai <rickoshade1891@gmail.com>
2024-06-12 14:32:19 +0530

    dsdsds

--- supply-chain/src/pages/index.tsx
+++ supply-chain/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Layout from './layout';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { Button } from '../components/ui/button';
 import { Card } from '../components/ui/card';
 
 const HomePage = () => {
